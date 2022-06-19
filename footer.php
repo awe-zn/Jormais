c9193bfee0dc840ed97334f558d36ccde3e19416
Zadoque Teófilo <zadoque.dev@gmail.com>
2022-06-19 19:08:06 -0300

    fix(footer): Modificação do background e icones

--- footer.php
+++ footer.php
@@ -1,34 +1,36 @@
-  <footer class="container mt-awe-56 pb-awe-80">
-    <div class="pt-awe-32 border-top border-1 border-light-silver">
-      <div class="d-flex gap-awe-24">
-        <a href="https://twitter.com/Jormais_portal" target="_blank" title="twitter">
-          <img src="<?php echo get_template_directory_uri(); ?>/dist/img/icons/twitter.svg" alt="link do twitter">
-        </a>
-        <a href="https://www.instagram.com/jor.mais/" target="_blank" title="instagram">
-          <img src="<?php echo get_template_directory_uri(); ?>/dist/img/icons/instagram.svg" alt="link do instagram">
-        </a>
-      </div>
-      <div class="row pt-awe-24">
-        <div class="col-12 col-md-6 col-lg-5">
-          <p class="text-old-silver fz-16">
-            Jormais - 2022 / Desenvolvido por
-            <a href="https://agenciaweb.ifrn.edu.br/" target="_blank" class="fw-bold text-old-silver text-decoration-none" title="Agência Web Escolar">
-              AWE
-            </a>
-          </p>
+  <footer class="bg-teal">
+    <div class="container mt-awe-56 pb-awe-56">
+      <div class="pt-awe-32">
+        <div class="row pt-awe-24">
+          <div class="col-12 col-md-6 col-lg-5">
+            <p class="text-white fz-16">
+              Jormais - 2022 / Desenvolvido por
+              <a href="https://agenciaweb.ifrn.edu.br/" target="_blank" class="fw-bold text-white text-decoration-none" title="Agência Web Escolar">
+                AWE
+              </a>
+            </p>
+          </div>
+          <div class="col-12 col-md-5 col-lg-7">
+            <nav class="d-flex gap-awe-16 gap-awe-md-24 justify-content-end flex-wrap flex-column flex-sm-row">
+              <a href="mailto:portaljormais@gmail.com" class="text-white text-decoration-none">
+                portaljormais@gmail.com
+              </a>
+            </nav>
+          </div>
         </div>
-        <div class="col-12 col-md-5 col-lg-7">
-          <nav class="d-flex gap-awe-16 gap-awe-md-24 justify-content-end flex-wrap flex-column flex-sm-row">
-            <a href="mailto:portaljormais@gmail.com" class="text-old-silver text-decoration-none">
-              portaljormais@gmail.com
-            </a>
-          </nav>
+        <div class="d-flex gap-awe-24">
+          <a href="https://twitter.com/Jormais_portal" target="_blank" title="twitter">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/img/icons/twitter-light.svg" alt="link do twitter">
+          </a>
+          <a href="https://www.instagram.com/jor.mais/" target="_blank" title="instagram">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/img/icons/instagram-light.svg" alt="link do instagram">
+          </a>
         </div>
       </div>
     </div>
   </footer>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
+  </body>
 
-</html>
+  </html>
