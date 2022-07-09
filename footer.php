6125c4df1577896a6e4473ece617663cd0645688
Zadoque Teófilo <zadoque.dev@gmail.com>
2022-07-09 13:24:11 -0300

    feat(footer): Atualização do footer

--- footer.php
+++ footer.php
@@ -1,14 +1,19 @@
-  <footer class="bg-teal">
-    <div class="container mt-awe-56 pb-awe-56">
+  <!-- <footer class="bg-teal">
+    <div class="container mt-awe-56 pb-awe-32">
       <div class="pt-awe-32">
         <div class="row pt-awe-24">
-          <div class="col-12 col-md-6 col-lg-5">
-            <p class="text-white fz-16">
-              Jormais - 2022 / Desenvolvido por
-              <a href="https://agenciaweb.ifrn.edu.br/" target="_blank" class="fw-bold text-white text-decoration-none" title="Agência Web Escolar">
-                AWE
+          <div class="col-12 col-md-6 col-lg-5 d-flex flex-column">
+            <div>
+              <img src="<?php echo get_template_directory_uri(); ?>/dist/img/logo-footer.png" alt="">
+            </div>
+            <div class="d-flex gap-awe-32 ps-awe-64 mt-awe-32">
+              <a href="https://twitter.com/Jormais_portal" target="_blank" title="twitter">
+                <img src="<?php echo get_template_directory_uri(); ?>/dist/img/icons/twitter-light.svg" alt="link do twitter">
               </a>
-            </p>
+              <a href="https://www.instagram.com/jor.mais/" target="_blank" title="instagram">
+                <img src="<?php echo get_template_directory_uri(); ?>/dist/img/icons/instagram-light.svg" alt="link do instagram">
+              </a>
+            </div>
           </div>
           <div class="col-12 col-md-5 col-lg-7">
             <nav class="d-flex gap-awe-16 gap-awe-md-24 justify-content-end flex-wrap flex-column flex-sm-row">
@@ -18,15 +23,54 @@
             </nav>
           </div>
         </div>
-        <div class="d-flex gap-awe-24">
-          <a href="https://twitter.com/Jormais_portal" target="_blank" title="twitter">
-            <img src="<?php echo get_template_directory_uri(); ?>/dist/img/icons/twitter-light.svg" alt="link do twitter">
+      </div>
+    </div>
+    <div class="d-flex justify-content-center py-4" style="background: #1E7780;">
+      <a href="https://agenciaweb.ifrn.edu.br/" target="_blank">
+        <img src="<?php echo get_template_directory_uri(); ?>/dist/img/icons/awe-logo.svg" alt="">
+      </a>
+    </div>
+  </footer> -->
+
+  <footer class="bg-teal">
+    <div class="container py-awe-32">
+      <div class="row">
+        <div class="col-12 col-md-4 col-lg-3">
+          <a href="<?php echo home_url('/'); ?>">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/img/logo-footer.png" class="img-fluid w-100" alt="Logo jormais">
           </a>
-          <a href="https://www.instagram.com/jor.mais/" target="_blank" title="instagram">
-            <img src="<?php echo get_template_directory_uri(); ?>/dist/img/icons/instagram-light.svg" alt="link do instagram">
+          <div class="d-flex gap-awe-32 justify-content-center pt-awe-32">
+            <a href="https://twitter.com/Jormais_portal" target="_blank" title="twitter">
+              <img src="<?php echo get_template_directory_uri(); ?>/dist/img/icons/twitter-light.svg" alt="link do twitter">
+            </a>
+            <a href="https://www.instagram.com/jor.mais/" target="_blank" title="instagram">
+              <img src="<?php echo get_template_directory_uri(); ?>/dist/img/icons/instagram-light.svg" alt="link do instagram">
+            </a>
+          </div>
+        </div>
+        <div class="col-12 col-md-4 col-lg-3 offset-lg-1 d-flex flex-column align-items-center">
+          <span class="text-white fw-bold fz-18 text-uppercase">APOIO INSTITUCIONAL</span>
+          <a href="https://www.ufrn.br/" target="_blank">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/img/ufrn-logo-light.png" alt="Logo ufrn">
+          </a>
+          <a href="">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/img/icons/logo-decom-light.svg" alt="Logo decom">
           </a>
         </div>
+        <div class="col-12 col-md-4 col-lg-4 offset-lg-1">
+          <span class="text-white fw-bold fz-18 text-uppercase">SOBRE JORMAIS</span>
+          <nav class="d-flex flex-column pt-awe-16 gap-awe-16">
+            <a href="" class="text-uppercase text-white fz-16 fw-light text-decoration-none">quem somos</a>
+            <a href="" class="text-uppercase text-white fz-16 fw-light text-decoration-none">contato</a>
+            <a href="" class="text-uppercase text-white fz-16 fw-light text-decoration-none">envie uma matéria</a>
+          </nav>
+        </div>
       </div>
+    </div>
+    <div class="d-flex justify-content-center py-4" style="background: #1E7780;">
+      <a href="https://agenciaweb.ifrn.edu.br/" target="_blank">
+        <img src="<?php echo get_template_directory_uri(); ?>/dist/img/icons/awe-logo.svg" alt="">
+      </a>
     </div>
   </footer>
 
