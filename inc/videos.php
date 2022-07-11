9eb0dad94a407007819ced5075daa1a256dc5bbe
Zadoque Teófilo Da Silva Maciel <m.zadoque@academico.ifrn.edu.br>
2022-07-11 14:00:50 -0300

    feat(index): Link para os vídeos no index

--- inc/videos.php
+++ inc/videos.php
@@ -3,10 +3,34 @@
     VÍDEOS
   </h2>
 
-  <div></div>
+  <style>
+    .video .play {
+      background: url(<?php echo get_template_directory_uri(); ?>/dist/img/icons/play.svg) no-repeat center center;
+    }
+
+    .video .play:hover {
+      background: url(<?php echo get_template_directory_uri(); ?>/dist/img/icons/play-hover.svg) no-repeat center center;
+    }
+  </style>
+
+  <?php
+  $video_args = array(
+    'post_type' => 'post_videos',
+    'posts_per_page' => '1'
+  );
+  $the_query = new WP_Query($video_args);
+  ?>
+
+  <?php if ($the_query->have_posts()) : while ($the_query->have_posts()) : $the_query->the_post(); ?>
+      <a href="<?php the_field('link'); ?>" target="_blank" class="video destaque">
+        <img src="<?php the_field('imagem_da_capa'); ?>" class="img-fluid" alt="">
+        <div class="filter"></div>
+      </a>
+  <?php endwhile;
+  else : endif; ?>
 
   <div class="pt-awe-32">
-    <a href="https://www.youtube.com/c/DecomUFRN" target="_blank" class="button-1">
+    <a href="<?php echo home_url('/videos'); ?>" class="button-1">
       Veja mais
     </a>
   </div>
